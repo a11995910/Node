92072a9cf48c0726e46ad374a8e6bc72bb55b2d5
wang <a11995910@wangs-MacBook-Pro.local>
2021-01-05 11:47:58 +0800

    addvue

---
+++ koa/koa2_app/datav/src/main.js
@@ -0,0 +1,11 @@
+import dataV from '@jiaminghi/data-view'
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(dataV)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
--- koa/koa2_app/routes/market.js
+++ koa/koa2_app/routes/market.js
@@ -14,21 +14,21 @@ router.get('/echarts1', async (ctx, next) => {
     console.log(selldata.nowDaySellData())
     ctx.body = await selldata.nowDaySellData()
 })
-// router.get('/echarts2', async (ctx, next) => {
-//     let data = null;
-//     ctx.body = await selldata.yesterdaySellDate()
-// })
-// router.get('/all_kinds', async (ctx) => {
-//     ctx.body = await selldata.allKindsDate()
-// })
-// router.get('/verbmonth', async (ctx) => {
-//     ctx.body = await selldata.verbmonth()
-// })
-// router.get('/nowdayKindsDate', async (ctx) => {
-//     ctx.body = await selldata.nowdayKindsDate()
-// })
-// router.get('/top10', async (ctx) => {
-//     ctx.body = await selldata.top10()
-// })
+router.get('/echarts2', async (ctx, next) => {
+    let data = null;
+    ctx.body = await selldata.yesterdaySellDate()
+})
+router.get('/all_kinds', async (ctx) => {
+    ctx.body = await selldata.allKindsDate()
+})
+router.get('/verbmonth', async (ctx) => {
+    ctx.body = await selldata.verbmonth()
+})
+router.get('/nowdayKindsDate', async (ctx) => {
+    ctx.body = await selldata.nowdayKindsDate()
+})
+router.get('/top10', async (ctx) => {
+    ctx.body = await selldata.top10()
+})
 
 module.exports = router
